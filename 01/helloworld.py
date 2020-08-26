e148250ffb8e8144b9511955c1dcc1fc3b8ad92f
heejucherish <sul1952@korea.ac.kr>
2020-08-26 16:07:45 +0900

    들여쓰기 연습

---
+++ 01/helloworld.py
@@ -0,0 +1,28 @@
+# helloworld.py
+
+print('hello world')
+
+a = 2
+if a< 1:  # 블록 시작(조건문)
+   b = 1
+    print(a)
+    print('big')
+    # 블록 끝 ( 조건문)
+b = 20
+
+def my_func(i):
+    print('새 블록 시작(함수정의')
+    if i > 10:
+        print('새블록 시작')
+        print('big')
+        print('...')
+        print('블록 끝(함수정의)')
+
+
+# 함수 호출
+my_func(20)
+
+
+
+
+
